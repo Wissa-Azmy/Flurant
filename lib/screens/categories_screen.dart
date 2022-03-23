ff9b8e85998a4b3eb59732c280ef36752642ed88
Wissa Michael <wissa@Wissas-iMac.local>
2022-03-23 18:43:10 +0100

    Add categories screen and categoryItem widget

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flurant/components/category_item.dart';
+import 'package:flurant/dummy_data.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatelessWidget {
@@ -5,13 +7,22 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Categories')),
+      body: GridView(
+        padding: const EdgeInsets.all(8),
+        children: dummy_categories
+            .map(
+              (category) =>
+                  CategoryItem(title: category.title, color: category.color),
+            )
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
