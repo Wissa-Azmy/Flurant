4bae7686bf588fee727eaf0e830dd40b568f9b12
Wissa Michael <wissa@Wissas-iMac.local>
2022-03-23 19:35:51 +0100

    add fonts

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -19,7 +19,10 @@ class CategoryItem extends StatelessWidget {
         ),
         borderRadius: BorderRadius.circular(15),
       ),
-      child: Text(title),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
     );
   }
 }
