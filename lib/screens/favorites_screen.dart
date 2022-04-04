0d4f5f7766ffd9a7061cf7058d3eb060cc905475
Wissa Michael <wissa.azmy@gmail.com>
2022-04-04 12:39:37 +0200

    implement favourites screen

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../components/meal_item.dart';
+import '../providers/meals_provider.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Favorites Screen'));
+    final favouriteMeals = Provider.of<MealsProvider>(context).favouriteMeals;
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ChangeNotifierProvider.value(
+        value: favouriteMeals[index],
+        child: MealItem(),
+      ),
+      itemCount: favouriteMeals.length,
+    );
   }
 }
