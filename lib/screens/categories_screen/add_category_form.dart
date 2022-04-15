840707724b73eca7f137d2b374c6697815e7e7b8
Wissa Michael <wissa.azmy@gmail.com>
2022-04-15 16:58:12 +0200

    Implement form submission and validation

--- lib/screens/categories_screen/add_category_form.dart
+++ lib/screens/categories_screen/add_category_form.dart
@@ -1,3 +1,4 @@
+import 'package:flurant/models/category.dart';
 import 'package:flutter/material.dart';
 
 class AddCategoryForm extends StatefulWidget {
@@ -10,48 +11,88 @@ class AddCategoryForm extends StatefulWidget {
 }
 
 class _AddCategoryFormState extends State<AddCategoryForm> {
-  final _priceFocusNode = FocusNode();
-  final _descriptionFocusNode = FocusNode();
+  final _priceNode = FocusNode();
+  final _descriptionNode = FocusNode();
+  final _formKey = GlobalKey<FormState>();
+  Map<String, String> _formValues = {};
+
+  void submit() {
+    final isValid = _formKey.currentState?.validate();
+    if (!isValid!) return;
+
+    _formKey.currentState?.save();
+
+    final category = Category(
+        id: DateTime.now().toString(),
+        title: _formValues['title'] ?? '',
+        color: Colors.white);
+
+    print('Submitted');
+    print(category.id);
+    print(category.title);
+  }
+
+  String? validateTitle(String? value) {
+    if (value!.isEmpty || value.length < 5) {
+      return 'Enter a valid title.';
+    }
+    return null;
+  }
+
+  String? validatePrice(String? value) {
+    if (value!.isEmpty) {
+      return 'Enter a valid Price.';
+    }
+    return null;
+  }
 
   @override
   void dispose() {
-    _priceFocusNode.dispose();
-    _descriptionFocusNode.dispose();
+    _priceNode.dispose();
+    _descriptionNode.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final _focusScope = FocusScope.of(context);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Add Category')),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
+            key: _formKey,
             child: ListView(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Title'),
-              textInputAction: TextInputAction.next,
-              onFieldSubmitted: (_) =>
-                  FocusScope.of(context).requestFocus(_priceFocusNode),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Price'),
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.number,
-              focusNode: _priceFocusNode,
-              onFieldSubmitted: (_) =>
-                  FocusScope.of(context).requestFocus(_descriptionFocusNode),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Description'),
-              maxLines: 3,
-              keyboardType: TextInputType.multiline,
-              focusNode: _descriptionFocusNode,
-            ),
-            OutlinedButton(onPressed: () {}, child: const Text('Submit'))
-          ],
-        )),
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Title'),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (_) => _focusScope.requestFocus(_priceNode),
+                  validator: validateTitle,
+                  onSaved: (value) =>
+                      _formValues.putIfAbsent('title', () => value ?? ''),
+                ),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Price'),
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.number,
+                    focusNode: _priceNode,
+                    onFieldSubmitted: (_) =>
+                        _focusScope.requestFocus(_descriptionNode),
+                    validator: validatePrice,
+                    onSaved: (value) =>
+                        _formValues.putIfAbsent('price', () => value ?? '')),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Description'),
+                    maxLines: 3,
+                    keyboardType: TextInputType.multiline,
+                    focusNode: _descriptionNode,
+                    onSaved: (value) => _formValues.putIfAbsent(
+                        'description', () => value ?? '')),
+                OutlinedButton(onPressed: submit, child: const Text('Submit'))
+              ],
+            )),
       ),
     );
   }
